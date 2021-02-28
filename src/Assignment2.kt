90c80900494ef29ce07c0fdb4ffb3901bd91400c
zillahmwimali <zillahmwimmali@gmail.com>
2021-02-28 13:20:12 +0300

    my 2nd assignment

---
+++ src/Assignment2.kt
@@ -0,0 +1,28 @@
+fun main() {
+   name()
+modulus()
+sum()
+    fact()
+}
+fun name(){
+    var name= "Hello Zillah"
+    print(name)
+}
+fun modulus(){
+    var num1=61
+    var num2=5
+    var modulus=num1%num2
+    print(modulus)
+}
+fun sum(){
+    var num1=61
+    var num2=5
+    var num3=17
+    var num4=4
+    var sum=(num1+num2+num3+num4)
+    print(sum)
+}
+fun fact(){
+    var fact= "I'm a protagonist"
+    print(fact)
+}
